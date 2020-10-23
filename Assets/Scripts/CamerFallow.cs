659c7606023b7a32726b73ba84dd7b2e3194c40e
Maxim_Kurbanov <max057246@gmail.com>
2020-10-23 13:57:34 +0300

    Сделал движение

---
+++ Assets/Scripts/CamerFallow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerFallow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = target.position - transform.position;
+    }
+    private void FixedUpdate()
+    {
+        transform.position = target.position - offset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    [SerializeField]
+    private float _speed;
+    private Vector3 _rightSpeed;
+    private bool isDead;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _rightSpeed = Vector3.right * _speed;
+        }
+        else
+        {
+            _rightSpeed = -Vector3.right * _speed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDead == false)
+        {
+            _rigidbody.MovePosition(transform.position + (Vector3.forward * _speed + _rightSpeed) * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Obstacle"))
+        {
+            Lose();
+        }
+    }
+
+    private void Lose()
+    {
+        isDead = true;
+    }
+}
